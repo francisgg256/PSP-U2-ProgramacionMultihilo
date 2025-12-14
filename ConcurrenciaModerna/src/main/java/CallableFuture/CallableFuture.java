adfaf5cabda21f42a3b8242fccaf1253d65c8797
francisgg256 <fjoseprohias@gmail.com>
2025-12-14 23:04:26 +0100

    Tema 2

--- ConcurrenciaModerna/src/main/java/CallableFuture/CallableFuture.java
+++ ConcurrenciaModerna/src/main/java/CallableFuture/CallableFuture.java
@@ -7,6 +7,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
+
 public class CallableFuture {
     public static void main(String[] args) {
         // 1. Control de Recursos: Solo permitimos 10 hilos simultáneos.
